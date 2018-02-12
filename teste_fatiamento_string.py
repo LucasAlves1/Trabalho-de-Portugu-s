25e9f328665f4135885a15e872d5a3c1a2dc547c
Lucas Alves <36404586+LucasAlves1@users.noreply.github.com>
2018-02-12 08:04:16 -0300

    Update teste_fatiamento_string.py

--- teste_fatiamento_string.py
+++ teste_fatiamento_string.py
@@ -1,2 +1,16 @@
 #-*-coding=utf-8 -*-
-
+fra='palavra'
+cont=len(fra)
+print(cont)
+ini=0
+lista=0
+soma=1
+fatias=[]
+while lista<cont:
+    ped=fra[ini:ini+soma]
+    fatias.append(ped)
+    ini+=1
+    lista+=1
+print(fatias)
+#CARALHO EU TO MUITO FELIZ :)
+#DEU CERTO DE PRIMEIRA
