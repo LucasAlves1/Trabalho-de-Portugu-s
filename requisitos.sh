cd87b849533ce1c7de8abb187b33f7cbb5df50fa
Lucas Alves <36404586+LucasAlves1@users.noreply.github.com>
2018-02-13 03:52:43 -0300

    Update requisitos.sh

--- requisitos.sh
+++ requisitos.sh
@@ -2,7 +2,23 @@
 clear
 echo Requisitos:
 echo
+echo 'PYTHON= 3.5'
+sudo apt-get install python3.5
+sudo apt-get update
+clear
+echo 'PIP'
+sudo apt-get install python-pip
+sudo apt-get update
+clear
+echo 'PIP3'
+sudo apt-get install python3-pip3
+sudo apt-get update
+clear
 echo 'KIVY >'
 sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get update
 sudo apt-get install python3-kivy
+
+
+echo 'EXECUÇÃO'
+python3.5 main.py
